1f7185ab8f2e204b40f4c0ce7bb7838571192ebd
luxagen <hello@luxagen.com>
2025-04-05 21:16:38 +0100

    IPC::Run

--- grm-new-repo.pl
+++ grm-new-repo.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use IPC::Run qw(run);
 
 # Check required environment variables
 my @missing;
@@ -23,9 +24,10 @@ my $virgin = !(-d ".git");
 my $grm_rpath_base = '/git/music-projects';
 
 # Get local and remote relative paths
-my $lrel = `grm list-lrel`;
+my ($lrel, $rrel);
+run(['grm', 'list-lrel'], '>', \$lrel);
 chomp($lrel);
-my $rrel = `grm list-rrel`;
+run(['grm', 'list-rrel'], '>', \$rrel);
 chomp($rrel);
 my $grm_rpath = "$grm_rpath_base/$rrel";
 $grm_rpath .= ".git" unless $grm_rpath =~ /\.git$/;
@@ -44,28 +46,35 @@ if ($reply !~ /^[Yy]$/) {
     exit 0;
 }
 
-system("git init -q");
-system($ENV{GRM_CFGCMD});
+# Initialize git repository
+run(['git', 'init', '-q']);
 
-# Set error handling mode
-$ENV{PERL_INLINE_C_FATAL_WARNINGS} = 1;  # Equivalent to set -e
+# Run the config command
+my @cfg_cmd = split /\s+/, $ENV{GRM_CFGCMD}; // TODO remove split
+run(\@cfg_cmd);
+
+# Set error handling mode (no direct equivalent in IPC::Run, handled by checking return values)
 
 # Create new remote repo based on remote template
-system("ssh $ENV{GRM_RLOGIN} \"cp -na --reflink=auto '$grm_rpath_base/$ENV{GRM_RPATH_TEMPLATE}' '$grm_rpath'\"");
+my $ssh_cmd = "cp -na --reflink=auto '$grm_rpath_base/$ENV{GRM_RPATH_TEMPLATE}' '$grm_rpath'";
+run(['ssh', $ENV{GRM_RLOGIN}, $ssh_cmd]);
 
 my $ssh_rpath = "ssh://$ENV{GRM_RLOGIN}$grm_rpath";
 
 # Check if remote exists, add or update it accordingly
-my $remote_exists = system("git remote get-url origin >/dev/null 2>&1") == 0;
+my $out;
+my $err;
+my $remote_exists = run(['git', 'remote', 'get-url', 'origin'], '>', \$out, '2>', \$err) ? 1 : 0;
+
 if ($remote_exists) {
     # Remote exists, update it
-    system("git remote set-url origin $ssh_rpath");
-    system("git fetch origin");
+    run(['git', 'remote', 'set-url', 'origin', $ssh_rpath]);
+    run(['git', 'fetch', 'origin']);
 } else {
     # Remote doesn't exist, add it
-    system("git remote add -f origin $ssh_rpath");
+    run(['git', 'remote', 'add', '-f', 'origin', $ssh_rpath]);
 }
 
 if ($virgin) {
-    system("git checkout master");
+    run(['git', 'checkout', 'master']);
 }
